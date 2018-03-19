82d4436271262c5566210d067faf3b5c88f4d999
junggeunhee <gh.jung1@albamapp.com>
2018-03-19 19:08:27 +0900

    Geuni:: 기존에 룰렛 휠 값 받았던거 제거, randomSymbol 객체로 뺌.

--- script/slotGame.js
+++ script/slotGame.js
@@ -1,150 +1,162 @@
-var game = (function () {
-  var userSlotMoney = 1000;
-  var gameMoney;
-  var resultSymbol = [];
-  var target;
+const slotSymbol = (() => {
+  let symbolList = [];
 
-  var clear = function() {
-    var removePoint = resultSymbol.length;
-    resultSymbol = resultSymbol.slice(removePoint);
-  };
-
-  var render = function() {
-    target.render(resultSymbol, userSlotMoney, gameMoney);
-  };
-  
-  var setLank = (function() {
-    var resultArr = [];
-    var userLank = 0;
-
-    return function() {    
-      var slotPattern = [
-        [7, 7, 7],
-        [7, 7, 6],
-        [7, 6, 7],
-        [7, 'ㅁ', 7],
-        [7, '*', 7],
-        ['#', 'M', '#'],
-        ['#', '#', '#']
-      ];
-      var lankSlotPattern = [
-        [7, 7, 7],
-        [7, 6, 7],
-        ['#', '#', '#'],
-        [7, 'ㅁ', 7],
-        ['#', 7, '#']
-      ];
-
-
-      var filterSlot = function(pattern, slots, matchedCb) {
-        var i, j, patternLen = pattern.length;
-        var resultSlot;
-        var patternSlot;
-
-        for (i = 0; i < patternLen; i++) {
-          resultSlot = [];
-          for (j = 0; j < pattern[i].length; j++) {
-            patternSlot = pattern[i][j];
-            if (patternSlot === slots[j]) resultSlot.push(patternSlot);
-            if (resultSlot.length === pattern[i].length) {
-              return matchedCb(resultSlot, i);
-            }
-          }
-        }
-      };
-
-      var getMatchSlotPattern = function(resultSlot, matchIndex) {
-        return resultSlot;
-      };
-      var getUserLank = function(resultSlot, matchIndex) {
-        return matchIndex;
-      };
-
-      resultArr = filterSlot(slotPattern, resultSymbol, getMatchSlotPattern);
-      // console.log('resultArr :', resultArr);
-      userLank = filterSlot(lankSlotPattern, resultArr, getUserLank);
-
-      switch (userLank) {
-        case 0:
-          console.log(userLank);
-          userSlotMoney += gameMoney * 4;
-          console.log(userSlotMoney);
-          break;
-        case 1:
-          console.log(userLank);
-          userSlotMoney += gameMoney * 3;
-          console.log(userSlotMoney);
-          break;
-        case 2:
-          console.log(userLank);
-          userSlotMoney += gameMoney * 2;
-          console.log(userSlotMoney);
-          break;
-        default:
-          console.log(userLank);
-          userSlotMoney += gameMoney;
-          console.log(userSlotMoney);
-          break;
-      }
-      render();
-      clear();
+  const setRandomSymbol = () => {
+    let randomCount = 0,
+        i = 0,
+        MAX = 3,
+        tmp = [];
+    for (i; i < MAX ; i++) {
+      randomCount = Math.floor(Math.random() * symbolList.length);
+      tmp.push(symbolList[randomCount]);
+      if (tmp.length === MAX) return tmp;
     }
-  })();
-
-  var setSlotSymbol = function (slotWheelCount) {
-    var testSymbol = ['7', '6', '*', 'ㅁ', '0', '#', 'M'];
-    var randomSlotIndex;
-    var i, j, symbolLen = testSymbol.length;
-
-    for (i = 0; i < symbolLen; i++) {
-      for (j = 0; j < slotWheelCount; j++) {
-        randomSlotIndex = Math.floor(Math.random() * symbolLen);
-      }
-      resultSymbol.push(testSymbol[randomSlotIndex]);
-    }
-    setLank();
   };
 
-  var init = function(money, wheel) {
-    target.init();
-    gameMoney = money;
-    userSlotMoney -= money;
-    setSlotSymbol(wheel);
-  };
-  
   return {
-    start: function() {    
-      var bettingMoney, userWheelCount;
-      if (!(confirm('슬롯머신 게임을 시작하겠습니까?'))) return;    
-      if (userSlotMoney === 0) return console.log('Money가 부족합니다.');
-      bettingMoney = prompt('판돈을 걸어 주세요!');
-
-      if (bettingMoney > userSlotMoney) {
-        console.log('현재 보유 머니 ( ' + userSlotMoney + ' ) 보다 판돈 ( ' + bettingMoney + ' ) 이 큽니다.');
-        return; 
-      }
-      userWheelCount = prompt('몇번 돌리실건가요?');                                     
-      init(bettingMoney, userWheelCount);      
+    setting: (symbol) => {
+      if (!(Array.isArray(symbol))) throw 'symbol List는 배열 형태로만 셋팅할 수 있습니다.';
+      symbolList = symbol;
+      return setRandomSymbol();
     },
-    setRenderer: function(renderer) {      
-      target = renderer;
-    } 
   }
 })();
 
-var con = (function () {
+const game = (() => {
+  let userSlotMoney = 1000;
+  let gameMoney;
+  let resultSymbol = [];
+  let target;
+
+  const clear = () => {
+    const removePoint = resultSymbol.length;
+    resultSymbol = resultSymbol.slice(removePoint);
+  };
+
+  const render = () => {
+    target.render(resultSymbol, userSlotMoney, gameMoney);
+  };
+
+  const lank = (() => {
+    let userLank;
+    return {
+      enrollment: () => {
+        const slotPattern = [
+          ['7', '7', '7'],
+          ['7', '7', '6'],
+          ['7', '6', '7'],
+          ['7', 'ㅁ', '7'],
+          ['7', '*', '7'],
+          ['#', 'M', '#'],
+          ['#', '#', '#']
+        ];
+        const lankSlotPattern = [
+          ['7', '7', '7'],
+          ['7', '6', '7'],
+          ['#', '#', '#'],
+          ['7', 'ㅁ', '7'],
+          ['#', '7', '#']
+        ];
+        let filterSlotSymbol;
+
+        const filterSlot = (pattern = [], slots = [], matchedCb) => {
+          for (let i = 0; i < pattern.length; i++) {
+            let cur;
+            let resultSlot = [];
+            for (let j = 0; j < slots.length; j++) {
+              cur = pattern[i][j];
+              if (cur === slots[j]) resultSlot.push(cur);
+              if (resultSlot.length === 3) {
+                return matchedCb(resultSlot, i);
+              }
+            }
+          }
+        };
+
+        const getMatchSlotPattern = function(resultSlot, matchIndex) {
+          return resultSlot;
+        };
+        const getUserLank = function(resultSlot, matchIndex) {
+          return matchIndex;
+        };
+
+        filterSlotSymbol = filterSlot(slotPattern, resultSymbol, getMatchSlotPattern);
+        userLank = filterSlot(lankSlotPattern, filterSlotSymbol, getUserLank);
+      },
+      rewardMoney: () => {
+        if (typeof userLank === 'undefined') {
+          render();
+          throw '순위권에 없습니다!';
+        }
+        switch (userLank) {
+          case 1:
+            userSlotMoney += gameMoney * 4;
+            break;
+          case 2:
+            userSlotMoney += gameMoney * 3;
+            break;
+          case 3:
+            userSlotMoney += gameMoney * 2;
+            break;
+          case 4 || 5:
+            userSlotMoney += gameMoney * 1;
+            break;
+        }
+        render();
+        clear();
+      }
+    }
+  })();
+
+
+  const setSlotSymbol = () => {
+    const symbolList = ['7', '6', '7', 'ㅁ', '#', 'M'];
+    resultSymbol = slotSymbol.setting(symbolList);
+    lank.enrollment();
+    lank.rewardMoney();
+  };
+
+  const init = (money) => {
+    target.init();
+    gameMoney = money;
+    userSlotMoney -= money;
+    setSlotSymbol();
+  };
+
+  return {
+    start: () => {
+      let bettingMoney;
+      if (!(confirm('슬롯머신 게임을 시작하겠습니까?'))) return;
+      if (userSlotMoney === 0) throw '판돈이 부족합니다.';
+      bettingMoney = parseInt(prompt('판돈을 걸어 주세요!'));
+      if (!bettingMoney) throw '판돈을 입력해주세요!';
+      if (bettingMoney > userSlotMoney) {
+        console.log('현재 보유 머니 ( ' + userSlotMoney + ' ) 보다 판돈 ( ' + bettingMoney + ' ) 이 큽니다.');
+        return;
+      }
+
+      init(bettingMoney);
+    },
+    setRenderer: (renderer) => {
+      target = renderer;
+    }
+  }
+})();
+
+const con = (() => {
   return {
     init: function() {
       console.clear();
     },
-    render: function(resultSymbol, userSlotMoney, gameMoney) {
+    render: (resultSymbol, userSlotMoney, gameMoney) => {
+      console.log(resultSymbol);
       console.group('Slot의 결과!');
       console.log('사용자의 게임 머니는 --> ', userSlotMoney);
       console.log('사용자가 배팅한 머니는 --> ', gameMoney);
-      resultSymbol.forEach(function(cur, index) {
+      resultSymbol.forEach((cur, index) => {
         console.log('사용자의 Slot값은 --> ' + index + ' : ' + cur );
       });
-      console.groupEnd();        
+      console.groupEnd();
     }
   }
 })();
